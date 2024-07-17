fb028f2fb685035a6b7c500176d844d8efe588bf
dmat913 <soccer.82.d.y@gmail.com>
2024-07-17 16:18:35 +0900

    fix: deploy

--- app/[type]/training/page.tsx
+++ app/[type]/training/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Training = () => {
-  return <div>Training</div>;
+// 型定義
+interface Params {
+  type: string;
+}
+
+// 静的パスを生成する関数
+export async function generateStaticParams(): Promise<Params[]> {
+  const paths: Params[] = [{ type: "level600" }];
+  return paths;
+}
+
+const Training = ({ params }: { params: Params }) => {
+  const { type } = params;
+  return <div>Training type: {type}</div>;
 };
 
 export default Training;
