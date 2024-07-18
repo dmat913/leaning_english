6afef2130c66eea50c345ecb4ac93cdd0842f2fb
dmat913 <soccer.82.d.y@gmail.com>
2024-07-18 18:50:40 +0900

    fix: redeploy

--- components/aceternity/Meteors.tsx
+++ components/aceternity/Meteors.tsx
@@ -4,9 +4,11 @@ import React from "react";
 export const Meteors = ({
   number,
   className,
+  disabled = false,
 }: {
   number?: number;
   className?: string;
+  disabled?: boolean;
 }) => {
   const meteors = new Array(number || 20).fill(true);
   return (
@@ -15,8 +17,10 @@ export const Meteors = ({
         <span
           key={"meteor" + idx}
           className={cn(
-            "animate-meteor-effect absolute top-1/2 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]",
-            "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent",
+            `${
+              !disabled && "animate-meteor-effect"
+            } absolute top-1/2 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]",
+            "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent`,
             className
           )}
           style={{
