a4519d9cdf2245e31c18abcbf5c8bd55abbf232d
dmat913 <soccer.82.d.y@gmail.com>
2024-07-21 09:09:45 +0900

    feat: level600単語全て追加

--- components/elements/DMATButton.tsx
+++ components/elements/DMATButton.tsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { CSSProperties, ReactElement } from "react";
 import { FaLocationArrow } from "react-icons/fa";
 
 const DMATButton = ({
   title,
   handleClick,
-  otherClasses,
+  otherClassesSpan,
+  otherClassesButton,
   disabled = false,
+  icon,
 }: {
   title: string;
   handleClick?: () => void;
-  otherClasses?: string;
+  otherClassesSpan?: CSSProperties;
+  otherClassesButton?: CSSProperties;
   disabled?: boolean;
+  icon?: ReactElement<any, any>;
 }) => {
   return (
     <button
       onClick={handleClick}
-      className="relative inline-flex overflow-hidden rounded-lg p-[1px] max-w-[240px] h-10 focus:outline-none disabled:opacity-[0.5]"
+      className={`relative inline-flex overflow-hidden rounded-lg p-[1px]  h-10 focus:outline-none disabled:opacity-[0.5]`}
+      style={otherClassesButton}
       disabled={disabled}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)] z-[1]" />
       <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg  px-3 py-1 text-sm font-medium text-white-1 bg-[#0D0D0D] gap-2 z-[2] ${otherClasses}`}
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg  px-3 py-1 text-sm font-medium text-white-1 bg-[#0D0D0D] gap-2 z-[2]`}
+        style={otherClassesSpan}
       >
         {title}
-        <FaLocationArrow />
+        {icon ?? <FaLocationArrow />}
       </span>
     </button>
   );
