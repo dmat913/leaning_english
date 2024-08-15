9e673a98c4c8653f1c167cf3c33d026bf261689c
dmat913 <soccer.82.d.y@gmail.com>
2024-08-15 17:13:12 +0900

    fix: api path

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  experimental: {
-    appDir: true,
-  },
   webpack(config, options) {
     config.module.rules.push({
       test: /\.(mp3)$/,
