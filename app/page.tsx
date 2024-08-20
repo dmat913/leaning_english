d962831f661844a11a4ac7dcdd9c20e82fe6150f
dmat913 <soccer.82.d.y@gmail.com>
2024-08-20 10:55:04 +0900

    fix: ローディング表示修正

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,15 @@ const LoginPage = () => {
   const [error, setError] = useState<string | null>(null);
   // ローディング状態
   const [loading, setLoading] = useState<boolean>(false);
+  // ログイン済み判定
+  const [isSuccessLogin, setIsSuccessLogin] = useState<boolean>(false);
 
   // ログインボタン押下時
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     // user取得
-    setLoading(true); // ローディング開始
+    setLoading(true);
+    setIsSuccessLogin(true);
     try {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/user?name=${encodeURIComponent(
@@ -34,9 +37,11 @@ const LoginPage = () => {
         router.push("/home");
       } else {
         setError(data.message);
+        setIsSuccessLogin(false);
       }
     } catch (err) {
       setError("エラーが発生しました。");
+      setIsSuccessLogin(false);
     } finally {
       setLoading(false);
     }
@@ -49,38 +54,40 @@ const LoginPage = () => {
           <GlassLoadingSpinner />
         </div>
       ) : (
-        <div className="flex items-center justify-center p-5 w-full h-full">
-          <div className="w-full max-w-sm p-8 bg-white shadow-md rounded-lg bg-white-1">
-            <h1 className="text-2xl font-semibold mb-6 text-center">
-              ログイン
-            </h1>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="username"
+        !isSuccessLogin && (
+          <div className="flex items-center justify-center p-5 w-full h-full">
+            <div className="w-full max-w-sm p-8 bg-white shadow-md rounded-lg bg-white-1">
+              <h1 className="text-2xl font-semibold mb-6 text-center">
+                ログイン
+              </h1>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="username"
+                >
+                  ユーザー名
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50"
+                  placeholder="DMAT"
+                  disabled={loading}
+                />
+              </div>
+              <button
+                className="w-full py-2 bg-blue-500 text-white-1 font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 disabled:opacity-50"
+                type="submit"
+                disabled={username.length === 0 || loading}
               >
-                ユーザー名
-              </label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50"
-                placeholder="DMAT"
-                disabled={loading}
-              />
+                ログイン
+              </button>
+              {error && <p style={{ color: "red" }}>{error}</p>}
             </div>
-            <button
-              className="w-full py-2 bg-blue-500 text-white-1 font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 disabled:opacity-50"
-              type="submit"
-              disabled={username.length === 0 || loading}
-            >
-              ログイン
-            </button>
-            {error && <p style={{ color: "red" }}>{error}</p>}
           </div>
-        </div>
+        )
       )}
     </form>
   );
--- components/elements/Loading.tsx
+++ components/elements/Loading.tsx
@@ -1,8 +1,14 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-const Loading = () => {
+const Loading = ({ otherClass }: { otherClass?: string }) => {
   return (
-    <div className="animate-spin h-10 w-10 border-4 border-[#FFD700] rounded-full border-t-transparent"></div>
+    <div
+      className={cn(
+        `animate-spin h-10 w-10 border-4 border-[#FFD700] rounded-full border-t-transparent`,
+        otherClass
+      )}
+    ></div>
   );
 };
 
